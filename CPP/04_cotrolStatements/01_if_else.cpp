82eeb09db6de8ae86075c2fe6d96e355d37821cc
Milan Sihag <milansihag11@gmail.com>
2023-08-03 01:10:49 +0530

    error fixing

--- CPP/04_cotrolStatements/01_if_else.cpp
+++ CPP/04_cotrolStatements/01_if_else.cpp
@@ -11,9 +11,9 @@ int main(){
     // else{
     //     cout << "Number is negative" << endl;
     // }
-    int userInput;
+    int userInput=4;
     cout << "Enter a number : ";
-    cin >> userInput;
+    // cin >> userInput;
     if(userInput%2==0){
         cout << "Number is even" << endl;
     }
--- CPP/datatype/datatype_intro.cpp
+++ CPP/datatype/datatype_intro.cpp
@@ -2,19 +2,19 @@
 #include<string>
 using namespace std;
 int main(){
-    int x = 5;
-    const int constant_value = 10; // this value can't be changed/override
-    double y = 5.5;
-    float z = 10.5;
-    char ch = 'a';
-    bool a = true;
-    string str = "milan";
-    cout << x << endl;
-    cout << y << endl;
-    cout << z << endl;
-    cout << ch << endl;
-    cout << str << endl;
-    cout << a << endl;
+    // int x = 5;
+    // const int constant_value = 10; // this value can't be changed/override
+    // double y = 5.5;
+    // float z = 10.5;
+    // char ch = 'a';
+    // bool a = true;
+    // string str = "milan";
+    // cout << x << endl;
+    // cout << y << endl;
+    // cout << z << endl;
+    // cout << ch << endl;
+    // cout << str << endl;
+    // cout << a << endl;
 
     int userInput;
     cout << "Enter a number : ";
@@ -30,8 +30,8 @@ int main(){
     cout << "Sum is : " << sumOfInputs << endl;
 
 
-    string name;
-    cout << "Enter your name : ";
-    getline(cin,name);
-    cout << "Name is : " << name << endl;
+    // string name;
+    // cout << "Enter your name : ";
+    // getline(cin,name);
+    // cout << "Name is : " << name << endl;
 }
