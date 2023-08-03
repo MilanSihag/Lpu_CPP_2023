70a7fc161cf1973032d8e595c751bd14fe881915
Milan Sihag <milansihag11@gmail.com>
2023-08-03 20:57:43 +0530

    patterns

--- CPP/04_cotrolStatements/04_for_loop.cpp
+++ CPP/04_cotrolStatements/04_for_loop.cpp
@@ -4,11 +4,24 @@ int main(){
 //	for(int i=100;i>=1;i-=2){
 //		cout<< i << " ";
 //	}
+
 	int row, column;
 	cin >> row >> column;
-	for(int i=1; i<=row; i++){
+//	for(int i=1; i<=row; i++){
+//		for(int j=1; j<=column; j++){
+//			cout<< "*";
+//		}
+//		cout << endl;
+//	}
+
+	for(int i=1; i<=row;i++){
 		for(int j=1; j<=column; j++){
-			cout<< "*";
+			if(i==1 || i==row || j==1 || j==column){
+				cout << "*" << " ";
+			}
+			else{
+				cout<< "  ";
+			}
 		}
 		cout << endl;
 	}
