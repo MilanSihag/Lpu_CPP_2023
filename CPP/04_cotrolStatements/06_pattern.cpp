04eec347e4fc207cf4c3e2b5aefed6d32ec57c79
Milan Sihag <milansihag11@gmail.com>
2023-08-05 20:28:44 +0530

    more pattern

--- CPP/04_cotrolStatements/06_pattern.cpp
+++ CPP/04_cotrolStatements/06_pattern.cpp
@@ -1,13 +1,23 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int row,col;
-	cin>>row>>col;
-	int k=1;
-	for(int i=1;i<=row;i++){
-		for(int j=col;j>=1;j--){
-			cout<< k<< " ";
-			k++;
-		}cout<<endl;
+//	int row,col;
+//	cin>>row>>col;
+//	int k=1;
+//	for(int i=1;i<=row;i++){
+//		for(int j=col;j>=1;j--){
+//			cout<< k<< " ";
+//			k++;
+//		}cout<<endl;
+//	}
+	int input;
+	cin>>input;
+	int i=1;
+	while(i<=input){
+		int j=1;
+		while(j<=i){
+			cout<<"*";
+			j++;
+		}i++; cout<<endl;
 	}
 }
