34fa07553886b409c97ca62bdb88c5171c944412
Milan Sihag <milansihag11@gmail.com>
2023-08-06 23:48:24 +0530

    hackerrank Qs

--- CPP/hackerrank_Qs.cpp
+++ CPP/hackerrank_Qs.cpp
@@ -4,11 +4,41 @@
 #include <iomanip>
 using namespace std;
 
-int main() {
-    // Complete the code.
-    int integer; long b; char a; float f; double d;
-    cin>> integer>>b>>a>>f>>d;
-    cout<<integer<<endl<<b<<endl<<a<<endl;
-    printf("%.3f\n%.9f",f,d);
+//int main() {
+//    // Complete the code.
+//    int integer; long b; char a; float f; double d;
+//    cin>> integer>>b>>a>>f>>d;
+//    cout<<integer<<endl<<b<<endl<<a<<endl;
+//    printf("%.3f\n%.9f",f,d);
+//    return 0;
+//}
+
+int main()
+{
+
+    int n;
+
+    // Write your code here
+    cin>>n;
+    if(n==1)
+    cout<<"one";
+    else if(n==2)
+    cout<<"two";
+    else if(n==3)
+    cout<<"three";
+    else if(n==4)
+    cout<<"four";
+    else if(n==5)
+    cout<<"five";
+    else if(n==6)
+    cout<<"six";
+    else if(n==7)
+    cout<<"seven";
+    else if(n==8)
+    cout<<"eight";
+    else if(n==9)
+    cout<<"nine";
+    else
+    cout<<"Greater than 9";
     return 0;
 }
