f5c9d86f9fdecc4811bf59bd66cf80b2665d635d
Milan Sihag <milansihag11@gmail.com>
2023-08-07 20:12:21 +0530

    non void functions

--- CPP/05_Functions/02_function_parameters.cpp
+++ CPP/05_Functions/02_function_parameters.cpp
@@ -9,9 +9,46 @@ using namespace std;
 void personDetails(string name,int marks){
 	cout<<"My name is "<<name<<" and i got "<<marks<<" in 12th"<<endl;
 }
+
+int product(int num1,int num2){
+	return num1*num2;
+}
+
+int maxNum(int num1,int num2,int num3){
+	int maximum;
+	if(num1>num2 && num1>num3){
+		maximum=num1;
+	}
+	else if(num2>num1 && num2>num3){
+		maximum = num2;
+	}
+	else{
+		maximum=num3;
+	}
+	return maximum;
+}
+
+int minNum(int num1,int num2,int num3){
+	int minimum;
+	if(num1<num2 && num1<num3){
+		minimum=num1;
+	}
+	else if(num2<num1 && num2<num3){
+		minimum = num2;
+	}
+	else{
+		minimum=num3;
+	}
+	return minimum;
+}
 int main(){
-//	printName();
+
+	int num1,num2,num3;
+	cin>>num1>>num2>>num3;
+	cout<<"maximum is: "<< maxNum(num1,num2,num3)<<" minimum is: "<<minNum(num1,num2,num3);
+	
+	//	printName();
 //	printName("World");
 //	fullName("Milan","Sihag");
-	personDetails("Milan",96);
+//	personDetails("Milan",96);
 }
