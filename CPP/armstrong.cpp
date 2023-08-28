4e3d59a55b3b692c9aef5a5d6e204738fd836b03
Milan Sihag <milansihag11@gmail.com>
2023-08-28 23:38:19 +0530

    armstrong number

--- CPP/armstrong.cpp
+++ CPP/armstrong.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
-inline isArmstrong(int number){
+inline bool isArmstrong(int number){
 	int sum=0,r,origNum=number,digits=0;
 	while(number>0){
 		number/=10;
@@ -9,6 +9,26 @@ inline isArmstrong(int number){
 	}
 	number=origNum;
 	while(number>0){
-		
+		r=number%10;
+		sum+=pow(r,digits);
+		number/=10;
 	}
+	return sum==origNum;
+}
+inline void printFac(int n){
+	for(int i=1;i<=n;i++){
+		if(n%i==0)
+		cout<<i<<" ";
+	}
+}
+int main(){
+	int n;
+	cin>>n;
+//	cout<<isArmstrong(n);
+	if(isArmstrong(n)){
+		cout<<n<<"is armstrong."<<endl;
+		printFac(n);
+	}
+	else
+	cout<<n<<"is not armstrong";
 }
