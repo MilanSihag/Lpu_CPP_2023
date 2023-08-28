18990e5e5d17d2debbbe83365dc46ea82b081691
Milan Sihag <milansihag11@gmail.com>
2023-08-28 21:03:27 +0530

    armstrong

---
+++ CPP/armstrong.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+inline isArmstrong(int number){
+	int sum=0,r,origNum=number,digits=0;
+	while(number>0){
+		number/=10;
+		digits++;
+	}
+	number=origNum;
+	while(number>0){
+		
+	}
+}
