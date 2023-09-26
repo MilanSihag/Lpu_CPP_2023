85713b2caa34155771a3eb964a477721105161f4
Milan Sihag <milansihag11@gmail.com>
2023-09-26 20:28:28 +0530

    hierarchial and hybrid, protected

---
+++ CPP/13_OOP/02_inheritance/04_hierarchial.cpp
@@ -0,0 +1,17 @@
+//hierarchial -: in this, we derive more than one child class from a base class
+#include<iostream>
+using namespace std;
+class Animation{
+	//running
+	//jumping
+};
+class Ch1: public Animation{
+	//fly
+};
+class Ch2: public Animation{
+	//swim
+};
+
+int main(){
+	
+}
---
+++ CPP/13_OOP/02_inheritance/06_protected.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+//protected -: like private members, protected members are inaccessible outside the class, however,
+//   they can be accessed derived class, friend classes/functions.
+//We need protected members if we want to hide the data of class, but still want that data to be
+//inherited by its derived class
+class Animal{
+	public:
+		void eat(){
+			cout<<"I can eat"<<endl;
+		}
+		void sleep(){
+			cout<<"I can sleep"<<endl;
+		}
+	protected:
+		string type;
+};
+class Dog: public Animal{
+	public:
+		void setType(string val){
+			type=val;
+		}
+		void display(){
+			cout<<"I am a "<<type<<endl;
+		}
+		void bark(){
+			cout<<"I can bark"<<endl;
+		}
+};
+int main(){
+	Dog d1;
+	d1.setType("pomerarian");
+	d1.display();
+}
