6d97451cbac0b471b01543ffef3913c2c20f9213
Milan Sihag <milansihag11@gmail.com>
2023-10-06 20:58:08 +0530

    binary tree

--- CPP/09_recursion/04_string_reversal.cpp
+++ CPP/09_recursion/04_string_reversal.cpp
@@ -2,20 +2,29 @@
 using namespace std;
 string rev(string str,int s,int e){
 	if(s>=e){
+//		cout<<str;
 		return str;
 	}
 	swap(str[s],str[e]);
 	rev(str,++s,--e);
 }
 string replace(string str,int s,int e){
-	if(s>=e) return str;
+	if(s>=e) return "\0";
 	if(str[s]=='p'&&str[s+1]=='i'){
-		str+=substr(0,)'3.14';
+		s+=2;
+		return "3.14"+replace(str,s,e);
+	}
+	else{
+		return str[s]+replace(str,++s,e); 
 	}
 }
 int main(){
-	string s="Hello";
-	cout<<rev(s,0,4);
-//	swap(s[0],s[4]);
-//	cout<<s;
+	string s1="Helloi";
+	int len=s1.length();
+//	string res1=rev(s1,0,len-1);
+//	cout<<res1<<endl;
+	string s2="pippipppi";
+	int n=s2.length();
+	string result=replace(s2,0,n-1);
+	cout<<result<<endl;
 }
