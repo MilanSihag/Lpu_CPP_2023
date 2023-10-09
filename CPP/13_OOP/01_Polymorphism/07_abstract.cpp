6057a02587c09f8f35ab9bdf7918ddc70d5d94a8
Milan Sihag <milansihag11@gmail.com>
2023-10-09 20:56:47 +0530

    abstraction

---
+++ CPP/13_OOP/01_Polymorphism/07_abstract.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+/*
+	Abstract class:
+	refers to class containing atleast one pure virtual function, which cannot be instantiated.
+	
+*/
+int main(){
+	
+}
