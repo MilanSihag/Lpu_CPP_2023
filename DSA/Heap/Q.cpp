29ea01fb554dac3583b647f0dc98ff5379195e65
Milan Sihag <milansihag11@gmail.com>
2023-10-27 20:55:44 +0530

    heap sort

---
+++ DSA/Heap/Q.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+class Node{
+	public:
+		int data;
+		Node* left;
+		Node* right;
+};
+bool isCBT(Node* root,int i, int nodeCount){
+	
+}
+bool isHeap(){
+	
+}
+int main(){
+	Node* root = NULL;
+	root->data = 10;
+	root->left->data = 8;
+	root->right->data = 9;
+	root->left->left->data = 7;
+	root->left->right->data = 6;
+
+}
